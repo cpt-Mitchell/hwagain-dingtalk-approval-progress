5d39fedf45e4ba6e318324e764b71a667816f207
cpt-Mitchell <67945997+cpt-Mitchell@users.noreply.github.com>
2021-10-25 11:11:11 +0800

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+export const DINTALK_API = true
+export const CORP_ID = 'dingc44594930dc9ab4635c2f4657eb6378f'
+export const API = {
+  DINGTALK_JSAPI: 'dingtalk/jsApi/getJsApiConf',
+
+  DINGTALK_USERID: 'dingtalk/user/getUserId',
+  DINGTALK_USERINFO: 'dingtalk/user/getUserInfo',
+  DINGTALK_GET_APPROVAL_PROGRESS: 'dingtalk/oa/common/transaction/submit'
+}
+
+export default {
+  DINTALK_API,
+  CORP_ID,
+  API
+}
